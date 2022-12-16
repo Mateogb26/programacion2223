8a694f5cb20739147ad5f098f43cbeaa2af969e5
Joaquin R <48088064+joaquinalbares@users.noreply.github.com>
2022-12-16 12:17:51 +0100

    Add files via upload

---
+++ UT03/ejercicioUno.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ejercicioUno {
+    public static void main(String[] args) {
+        int numeros[] = new int[10];
+        Scanner entrada = new Scanner(System.in);
+
+        for (int i=0; i<numeros.length ; i++) {
+            numeros[i] = entrada.nextInt();
+        }
+        System.out.println(Arrays.toString(numeros));
+
+        int numeroActual = numeros[numeros.length-1];
+        for (int i=numeros.length-1; i>=0; i--) {            
+            if (i>0) {                
+                numeros[i] = numeros[i-1];                
+            }
+        }
+        numeros[0] = numeroActual;
+        System.out.println(Arrays.toString(numeros));
+    }
+}
