fc39f582430127dc79658eb723a5059100f139fe
Joaquin R <48088064+joaquinalbares@users.noreply.github.com>
2023-03-27 13:47:49 +0200

    Update COMANDOS.sql

--- UT07/COMANDOS.sql
+++ UT07/COMANDOS.sql
@@ -2,7 +2,7 @@ create database usuarios;
 
 CREATE USER 'usuario'@'localhost' IDENTIFIED BY '1234';
 
-GRANT ALL PRIVILEGES ON database.usuarios TO 'usuario'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON usuarios.* TO 'usuario'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
 
